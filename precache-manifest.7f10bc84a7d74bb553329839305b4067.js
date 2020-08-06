89d6bfef428de6dbb20138c990550836eeae3ae6
146789 <34913079+146789@users.noreply.github.com>
2020-08-06 11:29:58 +0530

    Updates

---
+++ precache-manifest.7f10bc84a7d74bb553329839305b4067.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b95cb880902eab48c81138bc583d3a2a",
+    "url": "/react-webapp/index.html"
+  },
+  {
+    "revision": "35e211f7133eda405e01",
+    "url": "/react-webapp/static/css/main.b6033ad2.chunk.css"
+  },
+  {
+    "revision": "0db3f2cf024e14f92210",
+    "url": "/react-webapp/static/js/2.aed3938c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-webapp/static/js/2.aed3938c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35e211f7133eda405e01",
+    "url": "/react-webapp/static/js/main.5d5f2c93.chunk.js"
+  },
+  {
+    "revision": "afe5b6e0243411a9a98a",
+    "url": "/react-webapp/static/js/runtime-main.7941f60d.js"
+  },
+  {
+    "revision": "b94ef917d9096a805e4209063e93f347",
+    "url": "/react-webapp/static/media/profile15.b94ef917.png"
+  }
+]);
